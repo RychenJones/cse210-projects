7bb25ce7972020ee3a0cdb8f713d220413cb2ace
RychenJones <rychenjones66@gmail.com>
2026-06-13 17:45:02 -0600

    R

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,16 +6,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> list = new List<Shape>();
+
         Square newSqare = new Square("red", 5);
-        double area = newSqare.GetArea();
-        Console.WriteLine(area);
+        list.Add(newSqare);
+
+        // double area = newSqare.GetArea();
+        // Console.WriteLine(area);
 
         Rectangle newRec = new Rectangle("red", 5, 2);
-        double area2 = newRec.GetArea();
-        Console.WriteLine(area2);
+        list.Add(newRec);
+        // double area2 = newRec.GetArea();
+        // Console.WriteLine(area2);
 
         Circle newCircle = new Circle("red", 5);
-        double area3 = newCircle.GetArea();
-        Console.WriteLine(area3);
+        list.Add(newCircle);
+        // double area3 = newCircle.GetArea();
+        // Console.WriteLine(area3);
+        foreach (Shape i in list)
+        {
+            double area = i.GetArea();
+            Console.WriteLine(area);
+        }
     }
 }
